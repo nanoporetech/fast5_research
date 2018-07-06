8c064c9e28d7bbed7646aaabd4e3b6b915dd4fc2
Michael Wykes <michael.wykes@nanoporetech.com>
2018-07-06 15:39:28 +0100

    option for flat dir scruct and name by read_id

--- fast5_research/extract.py
+++ fast5_research/extract.py
@@ -1,10 +1,7 @@
 import argparse
 from concurrent.futures import ProcessPoolExecutor, as_completed
 import functools
-from functools import wraps
 import os
-import sys
-from timeit import default_timer as now
 
 import logging
 
@@ -18,9 +15,13 @@ def extract_single_reads():
         datefmt='%H:%M:%S', level=logging.INFO
     )
     logger = logging.getLogger('Extract Reads')
-    parser = argparse.ArgumentParser(description='Bulk .fast5 to single read .fast conversion.')
+    parser = argparse.ArgumentParser(description='Bulk .fast5 to single read .fast5 conversion.')
     parser.add_argument('input', help='Bulk .fast5 file for input.')
     parser.add_argument('output', help='Output folder.')
+    parser.add_argument('--flat', default=False, action='store_true',
+                        help='Create all .fast5 files in one directory')
+    parser.add_argument('--by_id', default=False, action='store_true',
+                        help='Name single-read .fast5 files by read_id.')
     parser.add_argument('--prefix', default='read', help='Read file prefix.')
     parser.add_argument('--channel_range', nargs=2, type=int, default=None, help='Channel range (inclusive).')
     parser.add_argument('--workers', type=int, default=4, help='Number of worker processes.')
@@ -33,7 +34,7 @@ def extract_single_reads():
 
     worker = functools.partial(
         extract_channel_reads,
-        args.input, args.output, args.prefix
+        args.input, args.output, args.prefix, args.flat, args.by_id,
     )
 
     if args.channel_range is None:
@@ -48,7 +49,7 @@ def extract_single_reads():
             for future in as_completed(futures):
                 try:
                     n_reads, channel = future.result()
-                except Exception as e:
+                except Exception:
                     logger.warning("Error processing channel.")
                 else:
                     logger.info("Extracted {} reads from channel {}.".format(n_reads, channel))
@@ -58,9 +59,14 @@ def extract_single_reads():
     logger.info("Finished.")
 
 
-def extract_channel_reads(source, output, prefix, channel):
-    out_path = os.path.join(output, str(channel))
-    os.makedirs(out_path)
+def extract_channel_reads(source, output, prefix, flat, by_id, channel):
+
+    if flat:
+        out_path = output
+    else:
+        out_path = os.path.join(output, str(channel))
+        os.makedirs(out_path)
+
     with BulkFast5(source) as src:
         raw_data = src.get_raw(channel, use_scaling=False)
         meta = src.get_metadata(channel)
@@ -97,9 +103,12 @@ def extract_channel_reads(source, output, prefix, channel):
                 }
 
                 raw_slice = raw_data[start:start+length]
-                filename = os.path.join(out_path, '{}_read_ch{}_file{}.fast5'.format(
-                    prefix, channel, read_number
-                ))
+                if by_id:
+                    filename = '{}.fast5'.format(read['read_id'])
+                else:
+                    filename = os.path.join(out_path, '{}_read_ch{}_file{}.fast5'.format(
+                        prefix, channel, read_number
+                    ))
                 with Fast5.New(filename, 'a', tracking_id=tracking_id, context_tags=context_tags, channel_id=channel_id) as h:
                     h.set_raw(raw_slice, meta=read_id, read_number=read_number)
     return counter, channel
