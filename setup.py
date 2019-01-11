478a558a89a8fab9012485651bea04b810b52cf0
Chris Wright <chris.wright@nanoporetech.com>
2019-01-11 13:35:14 +0000

    Add mread filtering

--- setup.py
+++ setup.py
@@ -55,7 +55,8 @@ setup(
     long_description_content_type=__long_description_content_type__,
     entry_points={
         'console_scripts': [
-            'extract_reads = {}.extract:extract_reads'.format(__pkg_name__)
+            'extract_reads = {}.extract:extract_reads'.format(__pkg_name__),
+            'filter_reads = {}.extract:filter_multi_reads'.format(__pkg_name__),
         ]
     },
     license='Mozilla Public License 2.0',
